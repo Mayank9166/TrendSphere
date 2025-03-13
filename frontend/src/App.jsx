6416cca24e893949ea759334b9d226a7cabf054f
Mayank9166 <mayankapply9166@gmail.com>
2025-03-14 01:45:41 +0530

    learn shadcn ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
     <div>
     <h1 className='text-2xl'>mayank saini</h1>
+    <Button className="bg-red-600">click me</Button>
     </div>
   )
 }
