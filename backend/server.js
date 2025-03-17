22d2c7ca05523b27720f0e2096ecec612bc4e233
Mayank9166 <mayankapply9166@gmail.com>
2025-03-17 13:57:00 +0530

    Database Setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import 'dotenv/config'
 
-const app = express();
+const app = express()
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Database is connected successFully");
+}).catch((err)=>{
+    console.log(err)
 
+});
 
-
-app.listen(3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log("Server is running on port 3000!");
 })
