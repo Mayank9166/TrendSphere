64fed1d12625f677ea2291f2fcbe02fb81055409
Mayank9166 <mayankapply9166@gmail.com>
2025-07-14 16:55:45 +0530

    user Post in admin in frontend

--- frontend/src/components/shared/DashboardPosts.jsx
+++ frontend/src/components/shared/DashboardPosts.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
+import { Link } from 'react-router-dom';
+
 
 const DashboardPosts = () => {
+  const {currentUser} = useSelector((state)=>state.user);
+  const [userPosts, setUserPosts] = React.useState([]);
+  console.log("User Posts:", userPosts);
+   useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`)
+        const data = await res.json();
+        if(res.ok)
+        {
+          setUserPosts(data.posts);
+
+        }
+      } catch (error) {
+        console.error('Error fetching posts:', error)
+      }
+    }
+    if(currentUser.isAdmin)
+    {
+      fetchPosts();
+    }
+   },[currentUser._id])
   return (
-    <div>
-     
+    <div className='flex flex-col items-center justify-center w-full p-3'>
+     {currentUser.isAdmin && userPosts.length > 0 ? (
+       <>
+       <Table>
+        <TableCaption>
+           A list of your published articles.
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+                <TableHead className="w-[200px]">Date Updated</TableHead>
+                <TableHead>Post Image</TableHead>
+                <TableHead>Post Title</TableHead>
+                <TableHead>Category</TableHead>
+                <TableHead>Delete</TableHead>
+                <TableHead>Edit</TableHead>
+          </TableRow>
+        </TableHeader>
+         {userPosts.map((post) => (
+              <TableBody className="divide-y">
+                 <TableRow key ={post._id}>
+                  <TableCell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>
+                      <Link to={`/post/${post.slug}`}>
+                      <img src={post.image} alt={post.title} className="w-20 h-20 object-cover bg-gray-500" />
+                      </Link>
+                  </TableCell>
+                  <TableCell>
+                      <Link to={`/post/${post.slug}`}>
+                       {post.title}
+                      </Link>
+                  </TableCell>
+                  <TableCell>
+                      <Link>
+                       {post.category}
+                      </Link>
+                  </TableCell>
+                  <TableCell>
+                      <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                        Delete
+                      </span>
+                      
+                  </TableCell>
+                  <TableCell>
+                        <Link to={`/update-post/${post._id}`} className='font-medium text-green-600 hover:underline cursor-pointer'>
+                        <span>Edit</span></Link>
+                  </TableCell>
+                 </TableRow>
+
+              </TableBody>
+         ))}
+       </Table>
+       </>
+  )  :(<p>You have no posts yet</p>)
+        }
     </div>
   )
 }
 
-export default DashboardPosts
+export default DashboardPosts;
