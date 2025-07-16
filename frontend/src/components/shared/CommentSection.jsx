ce2ccb9fdc32476771a0354ddf69ea6a6ce11a98
Mayank9166 <mayankapply9166@gmail.com>
2025-07-16 12:38:10 +0530

    Showing comments functionality

--- frontend/src/components/shared/CommentSection.jsx
+++ frontend/src/components/shared/CommentSection.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 import { Textarea } from '../ui/textarea';
 import { Button } from '../ui/button';
 import {toast} from 'sonner'
+import Comment from './Comment';
 const CommentSection = ({postId}) => {
     const {currentUser} = useSelector(state=>state.user);
     const [comment,setComment]=useState("")
-    
-    // console.log(currentUser);
+    const [allComments,setAllComments]=useState([]);
+    // console.log(allComments);
     const handleSubmit = async(e)=>{
       e.preventDefault();
       if(comment.length>200)
@@ -31,11 +32,12 @@ const CommentSection = ({postId}) => {
           )
 
         })
-        const data = res.json();
+        const data = await res.json();
         if(res.ok)
         {
           toast("Comment Successfully");
           setComment("");
+          setAllComments([data,...allComments]);
         }
         
       } catch (error) {
@@ -43,6 +45,22 @@ const CommentSection = ({postId}) => {
         toast("Something went wrong. Please Try again")
       }
     }
+    useEffect(()=>{
+      const getComments = async()=>{
+             try {
+                const res = await fetch(`/api/comment/getPostComments/${postId}` )
+                // console.log(res);
+                if(res.ok)
+                {
+                   const data = await res.json();
+                   setAllComments(data);
+                }
+             } catch (error) {
+              console.log(error);
+             }
+      }
+      getComments();
+    },[postId])
   return (
     <div className='max-w-3xl mx-auto w-full p-3'>
        {currentUser?(<div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
@@ -66,6 +84,18 @@ const CommentSection = ({postId}) => {
             </div>
           </form>
          )}
+
+         {allComments.length===0?(<p className='text-sm my-5'>No comments yet</p>):(<>
+         <div className='text-sm my-5 flex items-center gap-1 font-semibold'>
+          <p>Comments </p>
+          <div className='border border-gray-400 rounded-sm py-1 px-2'>
+            <p>{allComments.length}</p>
+          </div>
+         </div>
+         {allComments.map((comment)=>(
+          <Comment key={comment._id} comment={comment}/>
+         ))}
+         </>)}
     </div>
   
   )
