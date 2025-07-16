c0bd37ae407054c2c1b3f8e60c1b4147ed40cf81
Mayank9166 <mayankapply9166@gmail.com>
2025-07-16 15:13:22 +0530

    like the comment functionality complete

--- frontend/src/components/shared/Comment.jsx
+++ frontend/src/components/shared/Comment.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import moment from 'moment'
-const Comment = ({comment}) => {
+import { useSelector } from 'react-redux'
+import { AiFillLike } from "react-icons/ai";
+
+const Comment = ({comment,onLike}) => {
     const [user,setUser] = useState({})
+    const {currentUser} = useSelector((state)=>state.user)
     // console.log(user)
     useEffect(()=>{
         const getUser = async ()=>{
@@ -19,6 +23,7 @@ const Comment = ({comment}) => {
         getUser();
     },[comment])
   return (
+
     <div className='flex p-4 border-b border-slate-300 text-sm gap-2'>
         <div className='flex-shrink-0 mr-0'>
             <img src={user.profilePhotoUrl} alt={user.username} className='w-10 h-10 rounded-full bg-gray-200' />
@@ -30,6 +35,15 @@ const Comment = ({comment}) => {
 
                 </div>
                 <p className='text-slate-600 pb-2'>{comment.content}</p>
+                <div className='flex items-center pt-2 text-sm border-t border-slate-300 max-w-fit gap-2'>
+                  <button className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && "!text-blue-600"} `} type="button" onClick = {()=>onLike(comment._id)}>
+                    <AiFillLike className='text-lg'/>
+                  </button>
+                <p className='text-gray-400'>
+  {comment.numberOfLikes > 0 &&
+    `${comment.numberOfLikes} ${comment.numberOfLikes === 1 ? "like" : "likes"}`}
+</p>
+                </div>
             </div>
         </div>
       
