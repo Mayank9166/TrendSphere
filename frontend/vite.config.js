2f00d3d8071dadf90a720b49d6c718326a82c2f8
Mayank9166 <mayankapply9166@gmail.com>
2025-07-18 17:31:15 +0530

    change in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-      proxy:{
-        "/api":{
-          target:"http://localhost:3000",
-        }
-      }
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://trendsphere-3.onrender.com",
+        changeOrigin: true,
+        secure: false, // ⬅️ This disables SSL verification in dev
+      },
+    },
   },
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -18,4 +20,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
